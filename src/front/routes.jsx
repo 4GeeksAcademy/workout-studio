f708852ae44022f35abf0c64372ebad71409c187
Camilomunozjr <camilo.km@outlook.com>
2025-08-23 02:23:51 +0000

    ⚡️ Bug fixes

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -16,16 +16,7 @@ import ExercisesSection from "./components/ExercisesSection";
 export const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >
-<<<<<<< HEAD
-        <Route index element={<Home />} />
-        <Route path="single/:theId" element={<Single />} />
-        <Route path="demo" element={<Demo />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="login" element={<LogIn />} />
-        <Route path="test" element={<Test />} />
-        <Route path="test2" element={<Test2 />} />
-=======
-
+        
         {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
         <Route path= "/" element={<Home />} />
         <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
@@ -35,7 +26,7 @@ export const router = createBrowserRouter(
         <Route path="/test" element={<Test />} />
         <Route path="/test2" element={<Test2 />} />
         <Route path="/section" element={<ExercisesSection />} />
->>>>>>> 4063261 (:sparkles: Card implementation)
+
       </Route>
     )
 );
