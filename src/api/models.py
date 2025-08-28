bf39c888c37e1ad5cedc43418d69d8ba611aeb51
Ricardo <rick_fm_95@hotmail.com>
2025-08-28 02:33:33 +0000

    :bricks: db updated

--- src/api/models.py
+++ src/api/models.py
@@ -8,14 +8,30 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(50))
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    role:Mapped[str] = mapped_column(String(15), nullable=False, default="traine")
-    #is_trainer: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    password: Mapped[str] = mapped_column(String(120),nullable=False)
+    role: Mapped[str] = mapped_column(String(15), nullable=False, default="traine")
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    #Revisar cascade https://docs.sqlalchemy.org/en/20/orm/cascades.html
+
+    def __init__(self,name,email,password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    def save_user(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except:
+            return Exception("Something went wrong")
  
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
+            "password": self.password,
+            "role":self.role
             # do not serialize the password, its a security breach
         }
 
@@ -26,11 +42,29 @@ class Workout(db.Model):
     machine: Mapped[str] = mapped_column(nullable=False)
     link: Mapped[str] = mapped_column(String(255), nullable=True)
     media: Mapped[str] = mapped_column(String(255), nullable=True)
-    
+
+    def __init__(self,name,section,machine,link,media):
+        self.name = name
+        self.section = section
+        self.machine = machine
+        self.link = link
+        self.media = media
+
+    def update_workout(self):
+        try:
+            db.session.add(self)
+            db.session.commit() 
+        except:
+            return Exception("Something went wrong")
+
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "section": self.section,
+            "machine": self.machine,
+            "link": self.link,
+            "media": self.media
         }    
 
 
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Workout
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -12,11 +12,86 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
+
+def requestValues(body,keys):
+    for key in keys:
+        if key not in body:
+            raise APIException(f"{key} is not in body", status_code=400)
+        
+    return tuple(body.get(key) for key in keys )
+
 @api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+def handle_hello(): 
 
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
 
     return jsonify(response_body), 200
+
+"""
+This section is for User Endpoint
+"""
+@api.route('/user', methods=["GET"])
+def get_users():
+    users_list = User.query.all()
+    
+    return jsonify([
+        user.serialize() for user in users_list
+    ]), 200
+
+@api.route('/user', methods=["POST"])
+def create_user():
+    body = request.get_json()
+
+    name, email, password = requestValues(body, ["name","email","password"])
+    
+    try:
+        new_user = User(name=name,email=email,password=password)
+        new_user.save_user()
+    except:
+        return jsonify({"msg": "Something went wrong while adding to DB"}), 500
+
+    
+
+    return jsonify(new_user.serialize()), 201
+    
+
+@api.route('/user/<int:id>', methods=['GET'])
+def get_user_by_id(id):
+    user_by_id = User.query.get(id)
+    if not user_by_id:
+        return jsonify({"msg": f"The user that you are looking with the id:{id} is not on the DB"}), 404
+
+    return jsonify(user_by_id.serialize()), 200   
+
+@api.route('/user', methods=["DELETE"])
+def delete_user(id):
+    user_id = User.query.get(id)
+    db.session.delete(user_id)
+    db.session.commit()
+
+"""
+This section is for Workout Endpoint
+"""
+@api.route('/workout', methods=["POST"])
+def update_workout():
+    body = request.get_json()
+
+    name, section, machine, link, media = requestValues(body, ["name", "section", "machine", "link", "media"])
+
+    if not name or not section or not machine:
+        return jsonify({ "msg": "Error information is missing" }), 400
+
+    new_workout= Workout(name=name, section = section, machine  = machine, link = link, media = media )
+    new_workout.update_workout()
+
+    return jsonify(new_workout.serialize()), 201
+
+@api.route('/workout', methods=["GET"])
+def get_workouts():
+    workouts_list = Workout.query.all()    
+      
+    return jsonify(
+        [workout.serialize() for workout in workouts_list]
+    ), 201
